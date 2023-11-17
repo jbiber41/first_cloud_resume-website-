0a4afdaea87638ca3d16f795b478b3c299748e5f
jbiber41 <134236758+jbiber41@users.noreply.github.com>
2023-11-17 12:04:45 -0600

    Delete js/counter.js

--- js/counter.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = fetch("https://exzmevxguxooxxnbxu3tukgud40gkhxw.lambda-url.us-east-1.on.aws/");
-    let data = response.json();
-    counter.innerHTML = ` This page has ${data} Views!`;
-}
-
-updateCounter();
