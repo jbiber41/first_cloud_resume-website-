e21103f606c1d6f94031a47a122950f3b5e586ba
Jacob <jacob_biber@hotmail.com>
2023-11-17 12:00:12 -0600

    modified:   js/counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("Your-LambdaFunction-URL");
-    let data = await response.json();
+    let response = fetch("https://exzmevxguxooxxnbxu3tukgud40gkhxw.lambda-url.us-east-1.on.aws/");
+    let data = response.json();
     counter.innerHTML = ` This page has ${data} Views!`;
 }
 
